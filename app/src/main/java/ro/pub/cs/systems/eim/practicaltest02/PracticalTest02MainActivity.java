2f0578005a38632ecf7fe0ec2692b86d48e3e77e
Alexandra Mărăcine <alexandraa.maracine@gmail.com>
2021-06-03 12:31:58 +0300

    Added exercise 2 - graphic interface

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -3,12 +3,32 @@ package ro.pub.cs.systems.eim.practicaltest02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PracticalTest02MainActivity extends AppCompatActivity {
+
+    private EditText serverPortEditText = null;
+    private Button connectButton = null;
+
+    private EditText clientAddressEditText = null;
+    private EditText clientPortEditText = null;
+
+    private EditText currencyType = null;
+    private EditText response = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test02_main);
+
+        serverPortEditText = (EditText)findViewById(R.id.server_port_edit_text);
+        connectButton = (Button)findViewById(R.id.connect_button);
+
+        clientAddressEditText = (EditText)findViewById(R.id.client_address_edit_text);
+        clientPortEditText = (EditText)findViewById(R.id.client_port_edit_text);
+
+        currencyType = findViewById(R.id.currency);
+        response = findViewById(R.id.response);
     }
 }
